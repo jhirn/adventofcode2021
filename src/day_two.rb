ea9a238c7c37d8a67ec6e005cb4d17761017810f
Joe Hirn <joseph.hirn@gmail.com>
2021-12-03 16:19:01 -0600

    Day2 part 2 solution.

--- src/day_two.rb
+++ src/day_two.rb
@@ -1,3 +1,5 @@
+# Part 1
+
 [0, 0].tap do |(x, y)|
   File.open('input/day_two.txt', 'r').each_line do |line|
     direction, distance = line.split
@@ -9,6 +11,24 @@
       y -= distance
     when 'down'
       y += distance
+    end
+  end
+  puts x * y
+end
+
+# Part 2
+[0, 0, 0].tap do |(x, y, aim)|
+  File.open('input/day_two.txt', 'r').each_line do |line|
+    direction, distance = line.split
+    distance = distance.to_i
+    case direction
+    when 'forward'
+      x += distance
+      y += distance * aim
+    when 'up'
+      aim -= distance
+    when 'down'
+      aim += distance
     end
   end
   puts x * y
